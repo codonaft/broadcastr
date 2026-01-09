140cc2b44114106988efda23f606535a454e61c0
Alexander Lopatin <alopatindev@codonaft.com>
2026-01-09 19:52:19 +0400

    Increase graceful shutdown timeout

--- src/main.rs
+++ src/main.rs
@@ -297,7 +297,7 @@ async fn main() -> ah::Result<()> {
         }
     })
     .catch_signals()
-    .handle_shutdown_requests(Duration::from_millis(2000))
+    .handle_shutdown_requests(Duration::from_millis(4000))
     .await?;
     Ok(())
 }
